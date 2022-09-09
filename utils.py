7aa1206be5210a5e1a28645e233aef78da664f80
TosakRin <TosakRin@outlook.com>
2022-09-09 13:37:03 +0800

    feat: commit utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project_name   ：GPEN 
+# @file_name      : utils.py.py
+# @Create_time    : 2022/9/9 009 PM 13:34
+# @Author         : TosakRin
+# @Email          : TosakRin@outlook.com
+# @IDE            : PyCharm
+# @Describe       : {描述内容}
