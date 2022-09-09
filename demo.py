a184dfeed74f9a1dd4ef0adf06beb6d8bcb37f53
TosakRin <TosakRin@outlook.com>
2022-09-09 18:38:12 +0800

    doc: make some annotations

--- demo.py
+++ demo.py
@@ -41,11 +41,11 @@ if __name__ == '__main__':
     # 3. load model
     processor = None
     if args.task == 'FaceEnhancement':
-        processor = FaceEnhancement(args, in_size=args.in_size, model=args.model, use_sr=args.use_sr,
+        processor = FaceEnhancement(args, in_size=args.in_size,
+                                    model=args.model,
+                                    use_sr=args.use_sr,
                                     device='cuda' if args.use_cuda else 'cpu')
-    elif args.task == 'Segmentation2Face':
-        processor = Segmentation2Face(in_size=args.in_size, model=args.model, is_norm=False,
-                                      device='cuda' if args.use_cuda else 'cpu')
+
     # 4. load images and process
     files = sorted(glob.glob(os.path.join(args.indir, '*.*g')))
     for n, file in enumerate(files[:]):
@@ -56,10 +56,10 @@ if __name__ == '__main__':
             print(filename, 'error')
             continue
         # img = cv2.resize(img, (0,0), fx=2, fy=2) # optional
-
+        # 5. enhance face
         img_out, orig_faces, enhanced_faces = processor.process(img, aligned=args.aligned)
 
-        # ----- save results images -----
+        # 6. save results images -----
         img = cv2.resize(img, img_out.shape[:2][::-1])
         cv2.imwrite(os.path.join(args.outdir, '.'.join(filename.split('.')[:-1]) + f'_COMP{args.ext}'),
                     np.hstack((img, img_out)))
